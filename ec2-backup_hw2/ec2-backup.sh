74e8970c955809e732ca60b3967e345fae85e9be
Manideep Reddy <manideepreddy1116@gmail.com>
2019-04-07 03:37:55 -0400

    edit python file

--- ec2-backup_hw2/ec2-backup.sh
+++ ec2-backup_hw2/ec2-backup.sh
@@ -9,7 +9,7 @@ avail_zone=`aws ec2 describe-volumes --volume-ids $varname --query 'Volumes[*].[
 
 #Ceate an instance  in the same AZ based on the above AZ of the volume.
 
-instance=`aws ec2 run-instances --image-id ami-569ed93c --count 1 --instance-type t1.micro --key-name ec2-backup --placement AvailabilityZone=$avail_zone`
+instance=`aws ec2 run-instances --image-id  ami-569ed93c --count 1 --instance-type t1.micro --key-name ec2-backup --output json --placement AvailabilityZone=$avail_zone`
 
 #Sleep until the instance is created.
 sleep 20s
@@ -18,7 +18,7 @@ sleep 20s
 #We are using awk command to print the 12th line content (Instance Id) and piping it to sed.
 #You can use sed to chop off the beginning double quote, keep what's in between them, and chop off the remaining quote + everything there after.
 
-disp=`echo $instance |  awk '{print $12}' | sed 's/^"\(.*\)".*/\1/'`
+disp=`echo $instance |  awk '{print $48}' | sed 's/^"\(.*\)".*/\1/'`
 
 
 echo Instance created: $disp
@@ -30,10 +30,10 @@ aws ec2 attach-volume --volume-id $varname --instance-id $disp --device /dev/sdf
 
 pubDNS=`aws ec2 describe-instances --instance-ids $disp --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text`
 
-#echo $pubDNS
+echo $pubDNS
 
 #Here we are passing commands to be executed on ec2-instance after ssh. We have got an error to run disklabel. So, we have also given the path of disklabel(which disklabel)
-ssh -T -o StrictHostkeyChecking=no root@$pubDNS  'echo $(uname -a); echo $(whoami); echo $(/sbin/disklabel xbd2)' 
+ssh  -o StrictHostkeyChecking=no root@$pubDNS  'echo $(uname -a); echo $(whoami); echo $(/sbin/disklabel xbd2)' 
 
 #Terminating the Instance.
 aws ec2 terminate-instances --instance-ids $disp
